2bf4c012f727bf704c2e9194f84acaf01907c262
Fadi2109 <fk.1996.5@gmail.com>
2023-04-05 20:39:06 +0300

    firstVersion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+import { initializeApp } from "firebase/app";
 export const environment = {
+  firebase : {
+    apiKey: "AIzaSyA64qH4sTqW2jU2a-fr8eEsJ1UMeSoiuFE",
+    authDomain: "exposethefuture-5a692.firebaseapp.com",
+    projectId: "exposethefuture-5a692",
+    storageBucket: "exposethefuture-5a692.appspot.com",
+    messagingSenderId: "113576060894",
+    appId: "1:113576060894:web:6228d53f8b32c1c6c45f4f"
+  },
   production: true
 };
+export const app = initializeApp(environment.firebase);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,20 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+import { initializeApp } from "firebase/app";
 export const environment = {
+  firebase : {
+    apiKey: "AIzaSyA64qH4sTqW2jU2a-fr8eEsJ1UMeSoiuFE",
+    authDomain: "exposethefuture-5a692.firebaseapp.com",
+    projectId: "exposethefuture-5a692",
+    storageBucket: "exposethefuture-5a692.appspot.com",
+    messagingSenderId: "113576060894",
+    appId: "1:113576060894:web:6228d53f8b32c1c6c45f4f"
+  },
   production: false
 };
 
+export const app = initializeApp(environment.firebase);
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
